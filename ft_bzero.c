a80ec40c40610198aa74550d2d8e91e9aa53220e
Kuon Tanigawa <ktanigaw@c1r13s8.42tokyo.jp>
2021-11-14 14:32:35 +0900

    strlcpy~strlcat update

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,49 +6,13 @@
 /*   By: ktanigaw <ktanigaw@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 20:42:47 by ktanigaw          #+#    #+#             */
-/*   Updated: 2021/11/14 08:28:33 by ktanigaw         ###   ########.fr       */
+/*   Updated: 2021/11/14 14:31:14 by ktanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <strings.h>
-#include <string.h>
-#include <stdio.h>
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
 	ft_memset(s, 0, n);
-}
-
-int	main(int argc, char *argv[])
-{
-	char	str[6];
-	char	str1[6];
-
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = 'd';
-	str[4] = 'e';
-	str1[0] = 'a';
-    str1[1] = 'b';
-	str1[2] = 'c';
-    str1[3] = 'd';
-    str1[4] = 'e';
-	printf("%s\n", str);
-	printf("%s\n", str1);
-	bzero(str, 2);
-	ft_bzero(str1, 2);
-	printf("%c\n", str[0]);
-	printf("%c\n", str[1]);
-	printf("%c\n", str[2]);
-	printf("%c\n", str[3]);
-	printf("%c\n", str[4]);
-	printf("%s\n", argv[0]);
-	printf("%c\n", str1[0]);
-	printf("%c\n", str1[1]);
-	printf("%c\n", str1[2]);
-	printf("%c\n", str1[3]);
-	printf("%c\n", str1[4]);
-	return (argc);
 }
---
+++ ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktanigaw </var/mail/ktanigaw>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/14 11:28:57 by ktanigaw          #+#    #+#             */
+/*   Updated: 2021/11/14 12:45:05 by ktanigaw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	srclen;
+
+	srclen = ft_strlen(src);
+	if (dst == NULL || src == NULL)
+		return (0);
+	if (srclen + 1 < dstsize)
+	{
+		ft_memcpy(dst, src, srclen + 1);
+	}
+	else if (dstsize != 0)
+	{
+		ft_memcpy(dst, src, dstsize - 1);
+		dst[dstsize - 1] = '\0';
+	}
+	return (srclen);
+}
